4038616f4e92ff44d4b887af1f77385558b87db4
glavatski <glavatski@local.reshenie.spb.ru>
2023-02-21 16:16:36 +0300

    Рефактор тайпера на функции

--- frontend/src/components/Typer.vue
+++ frontend/src/components/Typer.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { GlobalEvents } from "vue-global-events";
 import { useScoreStore } from "../store/scoreStore";
-import { ref, reactive } from "vue";
+import { ref, reactive, onMounted, watch } from "vue";
 
 import Spinner from "./Spinner.vue";
+import { MdTimetoleave } from "oh-vue-icons/icons";
 
 const { updateScore } = useScoreStore();
 
@@ -26,108 +27,195 @@ async function sendResultToDatabase(score: number) {
   }
 }
 
-class Text {
-  value: string | null = null;
-  typed: string[] = [];
-  buffer: string = "";
+// class Text {
+//   value: string | null = null;
+//   typed: string[] = [];
+//   buffer: string = "";
 
-  async updateText() {
-    this.value = null;
-    this.typed = [];
-    this.value = await getTextFromDatabase();
-    this.buffer = this.value;
-  }
+//   async updateText() {
+//     this.value = null;
+//     this.typed = [];
+//     this.value = await getTextFromDatabase();
+//     this.buffer = this.value;
+//   }
 
-  input(letter: string) {
-    if (this.value && this.value[0] === letter) {
-      this.typed.push(this.value[0]);
-      this.value = this.value.slice(1);
-    }
-  }
+//   input(letter: string) {
+//     if (this.value && this.value[0] === letter) {
+//       this.typed.push(this.value[0]);
+//       this.value = this.value.slice(1);
+//     }
+//   }
 
-  reset() {
-    this.typed = [];
-    this.value = this.buffer;
-  }
-}
+//   reset() {
+//     this.typed = [];
+//     this.value = this.buffer;
+//   }
+// }
 
-class Timer {
-  value: number;
-  interval: any;
+// class Timer {
+//   value: number;
+//   interval: any;
 
-  constructor() {
-    this.value = 0;
-  }
+//   constructor() {
+//     this.value = 0;
+//   }
 
-  start() {
-    this.value += 100;
-    this.interval = setInterval(() => (this.value += 100), 100);
-  }
+//   start() {
+//     this.value += 100;
+//     this.interval = setInterval(() => (this.value += 100), 100);
+//   }
 
-  reset() {
-    clearInterval(this.interval);
-    this.value = 0;
-  }
+//   reset() {
+//     clearInterval(this.interval);
+//     this.value = 0;
+//   }
 
-  getAverage(typedLength: number) {
-    return (typedLength / this.value) * 60000;
-  }
-}
+//   getAverage(typedLength: number) {
+//     return (typedLength / this.value) * 60000;
+//   }
+// }
 
-class Typer {
-  public text: Text;
-  public timer: Timer;
-  public average = 0;
-  public active = false;
-  public loading = false;
-  public ready = false;
+// class Typer {
+//   public text: Text;
+//   public timer: Timer;
+//   public average = 0;
+//   public active = false;
+//   public loading = false;
+//   public ready = false;
 
-  constructor() {
-    this.text = reactive(new Text()); // Иначе не работает ререндер при обновлении свойств
-    this.timer = new Timer();
-    this.text.updateText();
-  }
+//   constructor() {
+//     this.text = reactive(new Text()); // Иначе не работает ререндер при обновлении свойств
+//     this.timer = new Timer();
+//     this.text.updateText();
+//   }
 
-  handleInput(event: KeyboardEvent) {
-    if (!this.text.value)
-      throw new Error("Попытка обработать не полученный текст!");
-    if (!this.active) this.start();
-    this.text.input(event.key);
-    this.average = this.timer.getAverage(this.text.typed.length);
-    if (this.text.value.length < 1) this.submit();
-  }
+// handleInput(event: KeyboardEvent) {
+//   if (!this.text.value)
+//     throw new Error("Попытка обработать не полученный текст!");
+//   if (!this.active) this.start();
+//   this.text.input(event.key);
+//   this.average = this.timer.getAverage(this.text.typed.length);
+//   if (this.text.value.length < 1) this.submit();
+// }
 
-  start() {
-    this.active = true;
-    this.timer.start();
-  }
+//   start() {
+//     this.active = true;
+//     this.timer.start();
+//   }
 
-  async submit() {
-    this.loading = true;
-    this.timer.reset();
-    await sendResultToDatabase(Math.floor(this.average));
-    await this.text.updateText();
-    this.average = 0;
-    this.active = false;
-    this.loading = false;
-  }
+//   async submit() {
+//     this.loading = true;
+//     this.timer.reset();
+//     await sendResultToDatabase(Math.floor(this.average));
+//     await this.text.updateText();
+//     this.average = 0;
+//     this.active = false;
+//     this.loading = false;
+//   }
 
-  reset() {
-    this.active = false;
-    this.average = 0;
-    this.timer.reset();
-    this.text.reset();
-  }
-}
-const typer = ref(new Typer());
+//   reset() {
+//     this.active = false;
+//     this.average = 0;
+//     this.timer.reset();
+//     this.text.reset();
+//   }
+// }
 
+// const typer = ref(new Typer());
+
+// Refs
+const time = ref(0);
 const text = ref("");
-setTimeout(() => {
-  text.value = typer.value.text.value || "text";
-}, 2000);
+const loading = ref(false);
+const inputCounter = ref(0);
+const average = ref(0);
+
+// watch(inputCounter, (newValue) => {
+//   average.value = newValue ? (newValue / time.value) * 60000 : 0;
+// });
+watch(time, (newValue) => {
+  average.value = newValue ? (inputCounter.value / newValue) * 60000 : 0;
+});
+
+// timer
+let interval: ReturnType<typeof setInterval> | null = null;
+
+function startTimer() {
+  time.value = 100;
+  interval = setInterval(() => (time.value += 100), 100);
+}
+
+function stopTimer() {
+  if (interval) {
+    time.value = 0;
+    clearInterval(interval);
+  }
+}
+
+// typer
+async function fetchText() {
+  const data = await getTextFromDatabase();
+  text.value = data;
+}
+
+async function submitResults() {
+  loading.value = true;
+  await sendResultToDatabase(average.value);
+  await fetchText();
+  resetInput();
+  loading.value = false;
+}
+
+function handleInput(event: KeyboardEvent) {
+  if (time.value === 0) startTimer();
+
+  if (inputCounter.value === text.value.length - 1) {
+    submitResults();
+  }
+
+  if (event.key === text.value[inputCounter.value]) {
+    inputCounter.value++;
+  }
+}
+
+function resetInput() {
+  inputCounter.value = 0;
+  stopTimer();
+}
+
+onMounted(() => {
+  loading.value = true;
+  fetchText().then(() => (loading.value = false));
+});
 </script>
 
 <template>
+  <div v-if="loading" class="loader">loading...</div>
+
+  <div v-else>
+    <div class="input">
+      <GlobalEvents
+        @keypress="handleInput($event)"
+        @keydown.esc="resetInput()"
+      />
+
+      time: {{ time }} <br />
+      avg: {{ average.toFixed() }} <br />
+
+      <span
+        v-for="(letter, index) in text"
+        :class="{
+          typed: index < inputCounter,
+          untyped: index > inputCounter,
+          cursor: index === inputCounter,
+        }"
+        >{{ letter }}</span
+      >
+    </div>
+  </div>
+</template>
+
+<!-- <template>
   <div class="typer" v-if="!typer.loading">
     <GlobalEvents
       @keypress="typer.handleInput($event)"
@@ -156,7 +244,7 @@ setTimeout(() => {
   <div v-else><Spinner /> Получаем новый текст...</div>
 
   <div v-else><Spinner /> Отправляем результат...</div>
-</template>
+</template> -->
 
 <style scoped lang="scss">
 .input {
