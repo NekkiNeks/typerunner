aafaa0ce98c3c0a8f945cabee55fda93910c2c5e
glavatski <glavatski@local.reshenie.spb.ru>
2023-04-14 13:21:50 +0300

    Замена длинного тире в тексте на короткое
    + Функция replaceLongDash() в Controllers/Text.ts заменяет длинные тире
      в тексте на короткие.

--- backend/Controllers/Text.ts
+++ backend/Controllers/Text.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { ServerError } from "../utils/Errors";
 
+function replaceLongDash(text: string): string {
+  return text.replace("—", "-");
+}
+
 export async function getText() {
   try {
     const data = await axios.get(
       "https://fish-text.ru/get?type=sentence&number=1"
     );
     if (data.data.status !== "success")
-      throw new Error("Ошибка обращения к стороннему API");
-    return data.data.text;
+    throw new Error("Ошибка обращения к стороннему API");
+
+    const text = data.data.text;
+    return replaceLongDash(text);
   } catch (err: any) {
     throw new ServerError(err.message);
   }
